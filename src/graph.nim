b06b2c2a569b60afb0a5cdd3ea268e81ba8b1d34
Erik G. Brandt <erik.brandt@cubist.eu>
2020-01-01 15:57:42 +0100

    Finished basic usage and test suite

--- src/graph.nim
+++ src/graph.nim
@@ -1,23 +1,38 @@
 import json
 import tables
 import oids
+import strformat
+
+export json
+export tables
 
 type
-  Edge = object
-    oid: string
-    label: string
-    properties: JsonNode
-    goingFrom: Node
-    goingTo: Node
+  Edge = ref object
+    oid*: string
+    label*: string
+    startsAt: Node
+    endsAt: Node
+    properties*: JsonNode
 
-  Node = object
-    oid: string
-    label: string
-    properties: JsonNode
-    edges: Table[string, Edge]
+  Node = ref object
+    oid*: string
+    label*: string
+    properties*: JsonNode
+    adj: Table[string, Edge]
 
   Graph = ref object
-    nodes: Table[string, Node]
+    name*: string
+    nodes*: Table[string, Node]
+    edges*: Table[string, Edge]
+
+proc `$`*(g: Graph): string =
+  result = fmt"I am {g.name}!"
+
+proc `$`*(n: Node): string =
+  result = fmt"<Node {n.oid} ({n.label}): {n.properties}>"
+
+proc `$`*(e: Edge): string =
+  result = fmt"<Edge {e.oid}: {e.startsAt.oid} -- {e.label} {e.properties} --> {e.endsAt.oid}>"
 
 proc `%`(t: tuple): JsonNode =
   ## Quick wrapper around the generic JObject constructor.
@@ -28,15 +43,25 @@ proc `%`(t: tuple): JsonNode =
 
 proc contains*(self: Graph, key: string): bool =
   ## Check if node oid is in Graph
-  result = key in self.nodes
+  result = key in self.nodes or key in self.edges
 
 proc contains*(self: Graph, key: Node): bool =
-  ## Check if node is in Graph
-  result = key.oid in self.nodes
+  ## Check if node object is in Graph
+  result = key.oid in self.nodes or key.oid in self.edges
 
-proc initNode*(label: string, properties: tuple = (),
+proc newGraph*(name: string = "graph"): Graph =
+  ## Create empty graph
+  new result
+  result.name = name
+
+proc newNode*(label: string, properties: tuple = (),
     ident: string = ""): Node =
-  ## Initialize a new Node
+  ## Create a new Node
+  new result
+
+  result.label = label
+  result.properties = %properties
+
   # Generate oid
   let oid =
     if ident.len == 0:
@@ -44,40 +69,68 @@ proc initNode*(label: string, properties: tuple = (),
     else:
       ident
 
-  result = Node(label: label, properties: %properties, oid: oid)
+  result.oid = oid
 
-proc newGraph*(): Graph =
-  ## Create empty graph
+proc newEdge*(A: Node, B: Node, label: string, properties: tuple = (),
+  ident: string = ""): Edge =
+  ## Create a new Edge
   new result
+
+  result.startsAt = A
+  result.endsAt = B
+  result.label = label
+  result.properties = %properties
+
+  # Generate oid
+  let oid =
+    if ident.len == 0:
+      $genOid()
+    else:
+      ident
+
+  result.oid = oid
 
 proc addNode*(self: var Graph, label: string, props: tuple = (),
     ident: string = ""): string =
   ## Add node to Graph
-  let n = initNode(label, properties = props, ident = ident)
+  let n = newNode(label, properties = props, ident = ident)
   self.nodes[n.oid] = n
   result = n.oid
 
 proc addNode*(self: var Graph, n: Node, props: tuple = ()): string =
-  ## Add node to Graph.  TODO: Update props
+  ## Add node to Graph.  TODO: Update properties
   self.nodes[n.oid] = n
   result = n.oid
 
-proc addEdge*[T, U](self: var Graph, first: T, second: U, label: string,
+proc addEdge*(self: var Graph, A: Node, B: Node, label: string,
     props: tuple = (), ident: string = ""): string =
-  ## Add edge to Graph #TODO: add properties
+  ## Add edge to Graph TODO: Update properties
   # Add nodes to graph if not already there
-  if first notin self:
-    self.addNode(first)
-  if second notin self:
-    self.addNode(second)
-  # Generate oid
-  let oid =
-    if ident.len == 0:
-      $genOid()
+  let idA =
+    if A in self:
+      A.oid
     else:
-      ident
+      self.addNode(A)
+  let idB =
+    if B in self:
+      B.oid
+    else:
+      self.addNode(B)
 
-  result = ""
+  let e = newEdge(A, B, label, properties = props, ident = ident)
+  self.nodes[A.oid].adj.add(B.oid, e)
+
+  result = e.oid
+
+proc addEdge*(self: var Graph, A: string, B: string, label: string,
+  props: tuple = (), ident: string = ""): string =
+  ## Add edge to Graph TODO: Update properties
+
+  let e = newEdge(self.nodes[A], self.nodes[B], label, properties = props, ident = ident)
+  self.nodes[A].adj.add(B, e)
+  self.edges[e.oid] = e
+
+  result = e.oid
 
 proc numberOfNodes*(self: Graph): int =
   ## Return number of nodes in Graph
@@ -85,10 +138,16 @@ proc numberOfNodes*(self: Graph): int =
 
 proc numberOfEdges*(self: Graph): int =
   ## Return number of edges in Graph
-  result = 0
+  result = self.edges.len
+
+proc hasEdge*(self: var Graph, A: string, B: string): bool =
+  result = A in self.nodes and B in self.nodes[A].adj
 
 when isMainModule:
-  var g = newGraph()
+  var g = newGraph("People")
+  let oid = g.addNode("Person")
 
-  let p1 = initNode("Person", (name: "John Doe", age: 24))
-  let p2 = initNode("Person", (name: "Jane Doe", age: 22))
+  # check oid in g
+  # check g.nodes[oid].label == "Person"
+  # check g.numberOfNodes == 1
+  # check g.numberOfEdges == 0
--- tests/test_graph.nim
+++ tests/test_graph.nim
@@ -1,39 +1,57 @@
 # To run these tests, simply execute `nimble test`.
-import json
 import graph
 import unittest
 
 suite "Basic usage":
-  test "create empty graph":
-    var g = newGraph()
+  test "create graph":
+    let g = newGraph("Test")
 
+    check g.name == "Test"
     check g.numberOfNodes == 0
     check g.numberOfEdges == 0
 
+  test "create nodes":
+    let p1 = newNode("Person", (name: "John Doe", age: 24), "new guy")
+    let p2 = newNode("Person", (name: "Jane Doe", age: 22))
+
+    check p1.label == "Person"
+    check p1.properties["name"].getStr() == "John Doe"
+    check p1.properties["age"].getInt() == 24
+    check p1.oid == "new guy"
+
+    check p2.label == "Person"
+    check p2.properties["name"].getStr() == "Jane Doe"
+    check p2.properties["age"].getInt() == 22
+
   test "add node with label":
-    var g = newGraph()
+    var g = newGraph("People")
     let oid = g.addNode("Person")
 
+    check oid in g
+    check g.nodes[oid].label == "Person"
     check g.numberOfNodes == 1
     check g.numberOfEdges == 0
 
   test "add node with label and properties":
-    var g = newGraph()
+    var g = newGraph("People")
     let oid = g.addNode("Person", (name: "John Doe", age: 24))
 
+    check oid in g
+    check g.nodes[oid].label == "Person"
+    check g.nodes[oid].properties["name"].getStr() == "John Doe"
+    check g.nodes[oid].properties["age"].getInt() == 24
     check g.numberOfNodes == 1
     check g.numberOfEdges == 0
-    check oid in g
 
   test "add edge with label and properties":
-    var g = newGraph()
-    let p1 = initNode("Person", (name: "John Doe", age: 24))
-    let p2 = initNode("Person", (name: "Jane Doe", age: 22))
+    var g = newGraph("People")
+    let p1 = g.addNode("Person", (name: "John Doe", age: 24))
+    let p2 = g.addNode("Person", (name: "Jane Doe", age: 22))
+    let r = g.addEdge(p1, p2, "MARRIED_TO", (since: 2012))
 
-    # let oid = g.addEdge(p1, p2, "MARRIED_TO", (since: 2012))
-
-    # check p1 in g
-    # check p2 in g
-    # check g.hasEdge(p1, p2)
-    # check g.numberOfNodes == 2
-    # check g.numberOfEdges == 1
+    check r in g
+    check g.edges[r].label == "MARRIED_TO"
+    check g.edges[r].properties["since"].getInt() == 2012
+    check g.hasEdge(p1, p2)
+    check g.numberOfNodes == 2
+    check g.numberOfEdges == 1
