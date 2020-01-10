3e2e4d89e6077f7508409aaa2b0f41b3e4bda824
Erik G. Brandt <erik.brandt@cubist.eu>
2020-01-10 23:23:12 +0100

    feat: Function to save graph as YAML

--- src/grim/io.nim
+++ src/grim/io.nim
@@ -1,10 +1,17 @@
+import os
+import oids
 import tables
 import grim/graph
 import grim/box
 import yaml
 import streams
+import sequtils
 import strutils
 import strformat
+from sugar import `=>`
+
+proc toYaml*[T](n: T): YamlNode =
+  newYamlNode(n)
 
 proc guessBox(s: string): Box =
   ## Return Box corresponding to (guessed) type contained in string
@@ -57,3 +64,51 @@ proc loadYaml*(fileName: string): Graph =
       edge["label"].content,
       edge["properties"].toTable
     )
+
+proc saveYaml*(g: Graph, fileName: string, force_overwrite: bool = false) =
+  if fileExists(fileName) and force_overwrite == false:
+    raise newException(IOError, fmt"Error: {fileName} exists, use force_overwrite flag to bypass.")
+
+  var
+    domName: string = g.name
+    domNodes: seq[YamlNode] = @[]
+    domEdges: seq[YamlNode] = @[]
+
+  for node in g.nodes.values:
+    var n = @[
+      ("label".toYaml, node.label.toYaml),
+      ("oid".toYaml, node.oid.toYaml)
+    ]
+
+    if node.properties.len > 0:
+      let v = toSeq(node.properties.pairs).map(x => (x[0].toYaml, ($x[1]).toYaml))
+      n.add(("properties".toYaml, v.toYaml))
+
+    domNodes.add(n.toYaml)
+
+  for edge in g.edges.values:
+    var e = @[
+      ("label".toYaml, edge.label.toYaml),
+      ("oid".toYaml, edge.oid.toYaml),
+      ("startsAt".toYaml, edge.startsAt.oid.toYaml),
+      ("endsAt".toYaml, edge.endsAt.oid.toYaml)
+    ]
+
+    if edge.properties.len > 0:
+      let v = toSeq(edge.properties.pairs).map(x => (x[0].toYaml, ($x[1]).toYaml))
+      e.add(("properties".toYaml, v.toYaml))
+
+    domEdges.add(e.toYaml)
+
+  var dom = initYamlDoc([
+    ("graph".toYaml, [
+      ("name".toYaml, domName.toYaml),
+      ("nodes".toYaml, domNodes.toYaml),
+      ("edges".toYaml, domEdges.toYaml)
+    ].toYaml
+    )
+  ].toYaml)
+
+  var strm = newFileStream(fileName, fmWrite)
+  dom.dumpDom(strm)
+  strm.close()
