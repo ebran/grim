a60f13b16b1722ee1dde0aad1527484c59229b6a
ebran <egbrandt02@gmail.com>
2020-01-15 22:16:37 +0100

    Polished code

--- tutorials/northwind.nim
+++ tutorials/northwind.nim
@@ -3,13 +3,22 @@ import sugar
 import grim
 import db_sqlite
 
-let
-  db = open("Northwind_small.sqlite", "", "", "")
-  headers = db.getAllRows(sql"SELECT name FROM PRAGMA_TABLE_INFO('Customer')").map(
+var
+  db: DbConn
+  headers: seq[string]
+  g: Graph
+
+# Initialize graph
+g = initGraph("northwind")
+
+# Read database
+db = open("Northwind_small.sqlite", "", "", "")
+
+# Read column names for customers
+headers = db.getAllRows(sql"SELECT name FROM PRAGMA_TABLE_INFO('Customer')").map(
     x => x[0])
 
-var g = initGraph("northwind")
-
+# Iterate over customers and add as nodes
 for row in db.fastRows(sql"SELECT * FROM Customer"):
   discard g.addNode("Customer", zip(headers, row.map(x => initBox(x))).toTable)
 
