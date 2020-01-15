e0df3eab7bee559669013dddecad29bcd6dce686
ebran <egbrandt02@gmail.com>
2020-01-15 22:12:30 +0100

    Use sqlite instead of postgresql, add customers as nodes in graph

--- tutorials/northwind.nim
+++ tutorials/northwind.nim
@@ -1,6 +1,16 @@
+import sequtils
+import sugar
 import grim
-import db_postgres
+import db_sqlite
+
+let
+  db = open("Northwind_small.sqlite", "", "", "")
+  headers = db.getAllRows(sql"SELECT name FROM PRAGMA_TABLE_INFO('Customer')").map(
+    x => x[0])
 
 var g = initGraph("northwind")
+
+for row in db.fastRows(sql"SELECT * FROM Customer"):
+  discard g.addNode("Customer", zip(headers, row.map(x => initBox(x))).toTable)
 
 echo g
