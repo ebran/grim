a91734075239189e1ad42388ac768c826bbd5260
ebran <egbrandt02@gmail.com>
2020-01-27 07:35:51 +0100

    Started shortest path experiments

--- tests/test_algo.nim
+++ tests/test_algo.nim
@@ -5,8 +5,20 @@ import unittest
 import grim
 
 suite "path algorithms":
+  setup:
+    var g = loadYaml("example.yaml")
+
   test "shortest path between two nodes":
     check 1 == 2
+    # let
+    #   self = toSeq(items(g.nodes("Person")))[0]
+    #   other = toSeq(items(g.nodes("Pet")))[0]
+
+    #   p = g.shortestPath(self, other)
+
+    # check p["name"] == "John":
+    # while (p = p.next; p.kind != nullBox):
+    #   check p["name"] == "Jane"
 
 suite "pattern matching":
   test "find nodes":
