ebf4296dff10fd4040d428a7d5b81c9c75df0f9a
ebran <egbrandt02@gmail.com>
2020-02-09 23:33:57 +0100

    Playing with http client

--- src/grim/neo4j.nim
+++ src/grim/neo4j.nim
@@ -6,30 +6,65 @@ import httpclient
 import base64
 import grim
 
+type
+  Neo4j* = object
+    client: HttpClient
+    address: string
+    auth: (string, string)
+
 proc getEnvOrRaise(env: string): string =
   if not os.existsEnv(env):
     raise newException(ValueError, "Environment variable $1 is not defined.".format(env))
   result = os.getEnv(env)
 
-let
-  user = getEnvOrRaise("NEO4J_USER")
-  password = getEnvOrRaise("NEO4J_PASSWORD")
-  hostname = getEnvOrRaise("NEO4J_HOSTNAME")
+proc initNeo4j(hostname: string, auth: (string, string)): Neo4j =
+  var client = newHttpClient()
+  client.headers["Authorization"] = "Basic " & base64.encode(auth[0] & ":" & auth[1])
+  result = Neo4j(client: client, address: hostname, auth: auth)
 
-  resource = Uri(
-    scheme: "http",
-    hostname: hostname,
-    port: "7474",
-    path: "db/data/transaction/commit"
-  )
+proc beginCommit(db: Neo4j, statement: string): Response =
+  let
+    # HTTP request
+    req = %*{
+      "statements": [
+        {
+        "statement": "MATCH (n)-[r]-() RETURN n,r",
+        "resultDataContents": ["graph"]
+        }
+      ]
+    }
+    # HTTP response
+    resp = db.client.post(db.address, body = $req)
 
-var client = newHttpClient()
-client.headers["Authorization"] = "Basic " & base64.encode(user & ":" & password)
+  result = resp
+
+proc dump(db: Neo4j): Graph =
+  let r = db.beginCommit("MATCH ()-[r]-() RETURN r")
+  echo r.version, ", ", r.status, ", ", r.headers #, ", ", r.body
+  result = newGraph()
 
 when isMainModule:
-  var data = %*{
-    "statements": [ {"statement": "MATCH (n) RETURN n"}]
-  }
+  let
+    user = getEnvOrRaise("NEO4J_USER")
+    password = getEnvOrRaise("NEO4J_PASSWORD")
+    hostname = getEnvOrRaise("NEO4J_HOSTNAME")
 
-  let resp = client.post($resource, body = $data)
-  echo resp.body
+    resource = Uri(
+      scheme: "http",
+      hostname: hostname,
+      port: "7474",
+      path: "db/data/transaction/commit"
+    )
+
+  var
+    db = initNeo4j($resource, auth = (user, password))
+    g = db.dump()
+
+  echo g
+
+  # var data = %*{
+  #   "statements": [ {"statement": "MATCH (n) RETURN n"}]
+  # }
+
+  # let resp = client.post($resource, body = $data)
+  # echo resp.body
