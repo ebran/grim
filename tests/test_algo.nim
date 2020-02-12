e4ce61b3e2891167346a944d7ce99267c1385f87
ebran <egbrandt02@gmail.com>
2020-02-12 22:16:07 +0100

    Disabled algorithm tests for now

--- tests/test_algo.nim
+++ tests/test_algo.nim
@@ -4,28 +4,28 @@ import unittest
 # grim imports
 import grim
 
-suite "path algorithms":
-  setup:
-    var g = loadYaml("example.yaml")
+# suite "path algorithms":
+#   setup:
+#     var g = loadYaml("example.yaml")
 
-  test "shortest path between two nodes":
-    check 1 == 2
-    # let
-    #   self = toSeq(items(g.nodes("Person")))[0]
-    #   other = toSeq(items(g.nodes("Pet")))[0]
+#   test "shortest path between two nodes":
+#     check 1 == 2
+#     # let
+#     #   self = toSeq(items(g.nodes("Person")))[0]
+#     #   other = toSeq(items(g.nodes("Pet")))[0]
 
-    #   p = g.shortestPath(self, other)
+#     #   p = g.shortestPath(self, other)
 
-    # check p["name"] == "John":
-    # while (p = p.next; p.kind != nullBox):
-    #   check p["name"] == "Jane"
+#     # check p["name"] == "John":
+#     # while (p = p.next; p.kind != nullBox):
+#     #   check p["name"] == "Jane"
 
-suite "pattern matching":
-  test "find nodes":
-    check 1 == 2
+# suite "pattern matching":
+#   test "find nodes":
+#     check 1 == 2
 
-  test "find edges":
-    check 1 == 2
+#   test "find edges":
+#     check 1 == 2
 
-  test "find relationships":
-    check 1 == 2
+#   test "find relationships":
+#     check 1 == 2
