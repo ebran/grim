e4563ee410e30a819911d9d83c747b7f2a9c2fe3
ebran <egbrandt02@gmail.com>
2020-02-19 09:15:53 +0100

    Started with test for paths

--- tests/test_paths.nim
+++ tests/test_paths.nim
@@ -1,28 +1,45 @@
 # stdlib imports
 import unittest
+import sequtils
+import sets
+import strutils
 
 # grim imports
 import grim
 import grim/utils
 
-suite "Paths":
-  setup:
-    var g = newGraph("test")
+var g = newGraph("Path tester")
 
-    discard g.addNode("Person", %(name: "Alice"), oid = "alice")
-    discard g.addNode("Person", %(name: "Bob"), oid = "bob")
-    discard g.addNode("Person", %(name: "Charlie"), oid = "charlie")
+# Persons
+discard g.addNode("Person", %(name: "Alice"), oid = "alice")
+discard g.addNode("Person", %(name: "Bob"), oid = "bob")
+discard g.addNode("Person", %(name: "Charlie"), oid = "charlie")
 
-    let
-      e1 = g.addEdge("alice", "bob", "KNOWS", %(since: 2012), oid = "alice-bob")
-      e2 = g.addEdge("bob", "charlie", "KNOWS", %(since: 2013),
-          oid = "bob-charlie")
+discard g.addEdge("alice", "bob", "KNOWS", %(since: 2012),
+    oid = "alice-bob")
+discard g.addEdge("bob", "charlie", "KNOWS", %(since: 2013),
+    oid = "bob-charlie")
 
-  test "create and walk path":
-    var
-      p = g.node("alice")
-        .newPath
-        .add(g.edge("alice-bob"))
-        .add(g.edge("bob-charlie"))
+# Adresses
+discard g.addNode("Address", %(street: "Anywhere Road 1",
+    zip: 11111), oid = "first house")
+discard g.addNode("Address", %(street: "Somewhere Street 1", zip: 11112),
+    oid = "second house")
 
-    check sequalizeIt(p.walk) == @[g.edge(e1), g.edge(e2)]
+# Residence chain
+discard g.addEdge("alice", "first house", "FIRST", %(
+    date: "2017-03-24"), oid = "to-first house")
+discard g.addEdge("alice", "first house", "FIRST", %(
+    date: "2017-03-24"), oid = "to-first house again")
+discard g.addEdge("first house", "second house", "MOVED_TO", %(
+    date: "2017-03-24"), oid = "first house-second house")
+
+when isMainModule:
+  var pc = g
+    .paths("Person")
+    .step("FIRST", "Address")
+
+  echo "There are $1 paths in the collection.".format(pc.len)
+
+  for path in pc:
+    echo path
